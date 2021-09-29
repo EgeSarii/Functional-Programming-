91efef0060a69da37764ac5a7b4276eee0c2f094
esari <Ege.Sari@ru.nl>
2021-09-29 14:11:48 +0200

    AST.h is finished

--- Assignment 4/AST.hs
+++ Assignment 4/AST.hs
@@ -1,11 +1,27 @@
+--Ege Sari s1034535
+--Group 81
+
 module AST where
 
---start with either:
---  data Expr = Lit Integer | Add Expr Expr | Mul Expr Expr | ...
---or:
---  data Expr = Lit Integer | Expr :+: Expr | Expr :*: Expr | ...
---  infixl 6 :+:
---  infixl 7 :*:
+data Expr = Lit Integer 
+          | VarX 
+          | Add Expr Expr 
+          | Mul Expr Expr 
+          | Sub Expr Expr 
+          | Div Expr Expr
+
+expr = Mul (VarX) (Mul (Lit 4) (Lit 2))
+          
+eval :: (Fractional a) => Expr -> a -> Maybe a
+eval (Lit a) x =  Just (fromIntegral a)
+eval (VarX) a = (Just a)
+eval (Add e1 e2) b = Just (head(fromVal(eval e1 b)) + head(fromVal(eval e2 b)))
+eval (Mul e1 e2) b = Just (head(fromVal(eval e1 b)) * head(fromVal(eval e2 b)))
+eval (Sub e1 e2) b = Just (head(fromVal(eval e1 b)) - head(fromVal(eval e2 b)))
+eval (Div e1 e2) b = if (eval(Mul e2 (VarX)) 0)== (Just 0.0) then Nothing else Just (head(fromVal(eval e1 b)) / head(fromVal(eval e2 b)))
 
 
---eval :: (Fractional a) => Expr -> a -> Maybe a
+
+fromVal :: Maybe a -> [a]
+fromVal Nothing = []
+fromVal (Just a) = [a]
