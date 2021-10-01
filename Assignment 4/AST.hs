f19de8906f62ddbe5200a1650712fd2cbcf75c47
esari <Ege.Sari@ru.nl>
2021-10-01 09:46:54 +0200

    Updated AST.hs

--- Assignment 4/AST.hs
+++ Assignment 4/AST.hs
@@ -11,12 +11,12 @@ data Expr = Lit Integer
           | Div Expr Expr
           
 eval :: (Fractional a, Eq a) => Expr -> a -> Maybe a
-eval (Lit a) x =  Just (fromIntegral a)
+eval (Lit a) _ =  Just (fromIntegral a)
 eval (VarX) a = (Just a)
 eval (Add e1 e2) b = Just (head(fromVal(eval e1 b)) + head(fromVal(eval e2 b)))
 eval (Mul e1 e2) b = Just (head(fromVal(eval e1 b)) * head(fromVal(eval e2 b)))
 eval (Sub e1 e2) b = Just (head(fromVal(eval e1 b)) - head(fromVal(eval e2 b)))
-eval (Div e1 e2) b = if (eval e2 0)== (Just 0) then Nothing else Just (head(fromVal(eval e1 b)) / head(fromVal(eval e2 b)))
+eval (Div e1 e2) b = if head(fromVal(eval e2 b))== 0 then Nothing else Just (head(fromVal(eval e1 b)) / head(fromVal(eval e2 b)))
 
 
 
