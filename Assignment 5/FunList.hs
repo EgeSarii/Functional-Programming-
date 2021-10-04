28e9dcbfcb9b363503911af26a57f99f0f80fb3f
esari <Ege.Sari@ru.nl>
2021-10-04 18:06:03 +0200

    FunList.hs finished

--- Assignment 5/FunList.hs
+++ Assignment 5/FunList.hs
@@ -13,12 +13,10 @@ compose' fs x = foldr compose x [fs]
 
  
 foo :: (Integral n) => n -> n
-foo n = compose (map (+) [1..n]) 1
---This function 
+foo n = compose (map (*) [1..n]) 1
+--This function does the exactly same what foldr (*) 1 [1..n] does. It gives the value of n! .
 
 --map :: (a->a) -> [a] -> [a]
 
 foldr' :: (a -> b -> b) -> b -> [a] -> b
-foldr' f a (x:xs) = a
-
-``
+foldr' f a xs = compose (map (f) xs) a
