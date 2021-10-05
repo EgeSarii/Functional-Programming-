ae15ba8bdd60cd64f887976aefd2c1b52c1f4790
esari <Ege.Sari@ru.nl>
2021-10-05 17:28:16 +0200

    Unfold.hs partially done

--- Assignment 5/Unfold.hs
+++ Assignment 5/Unfold.hs
@@ -3,10 +3,20 @@ module Unfold where
 import Data.List (unfoldr)
 import Prelude hiding (take,zip,(++))
 
--- bits :: Int -> [Int]
--- zip :: [a] -> [b] -> [(a,b)]
--- take :: Int -> [a] -> [a]
--- primes :: [Integer]
+
+bits :: Int -> [Int]
+bits n = reverse (dropWhile (<1) (convert n 62))
+
+convert :: Int -> Int -> [Int]
+convert s k = unfoldr( \(n,k) -> if (k== -1) then Nothing else (if (div n (2^k))==0 then Just (0, ((n),k-1)) else Just (1, ((n- 2^k), (k-1)))))(s,k) 
+
+zip :: [a] -> [b] -> [(a,b)]
+zip a b= unfoldr (\(as, bs) -> case as of [] -> Nothing ; (x:xs)-> (case bs of []-> Nothing; (y:ys)-> Just((x,y),(xs,ys)))) (a,b)
+
+take :: Int -> [a] -> [a]
+take n a = unfoldr (\(s,as)-> case as of [] -> Nothing ; (x:xs)-> (if s<1 then Nothing else Just(x, (s-1, xs)))) (n,a)
+
+--primes :: [Integer]
 
 apo :: (t -> Either [a] (a, t)) -> t -> [a]
 apo f seed = case f seed of
