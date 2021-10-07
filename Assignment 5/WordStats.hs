1eb0e44fc00337bde4e9884dcde6fde72ae00ca2
Adınız <siz@e-posta.com>
2021-10-08 00:03:01 +0200

    Nearly completed WordStats.hs

--- Assignment 5/WordStats.hs
+++ Assignment 5/WordStats.hs
@@ -7,10 +7,14 @@ wordFrequency :: String -> [(String,Int)]
 wordFrequency  = map (\x->(head x,length x)) . group . sort . words
 
 mostFrequentOfLength :: Int -> String -> [(String,Int)]
-mostFrequentOfLength n = (filter (\(x,y)-> y>=n)). wordFrequency 
---wordLengthFrequency :: String -> ??
+mostFrequentOfLength n = reverse.(filter (\(x,y)-> y>=n)). wordFrequency 
 
---anagrams :: String -> ??
+wordLengthFrequency :: String -> [(Int,Int)]
+wordLengthFrequency  = sortOn fst . map (\(x,y) -> ((length x), y))  . wordFrequency
+
+anagrams :: String -> [[String]]
+anagrams
+
 
 main :: IO ()
 main = onStdin $ wordFrequency  -- change this to run a different function
