a00f0d807f68807cc356f874156df346c3798c52
ESari <sariege323@gmail.com>
2021-11-16 20:59:02 +0100

    Updated Stream.hs

--- Assignment 9/Stream.hs
+++ Assignment 9/Stream.hs
@@ -7,34 +7,58 @@ data Stream a = a :> Stream a
 infixr 5 :>
 
 instance (Show a) => Show (Stream a) where
-  show s = "(" List.++ showN (16::Int) s List.++ ")"
+  show s = "(" List.++ showN (5::Int) s List.++ ")"
     where
     showN 0 _         = "..."
-    showN n (x :> xs) = show x List.++ "asdad :> " List.++ showN (n-1) xs
+    showN n (x :> xs) = show x List.++ " :> " List.++ showN (n-1) xs
 
 from :: Integer -> Stream Integer
 from n = n :> from (n + 1)
 
---head :: Stream a -> a
+head :: Stream a -> a
+head (a:> as) = a
 
---tail :: Stream a -> Stream a
+tail :: Stream a -> Stream a
+tail (a:> as) = as
 
---repeat :: a -> Stream a
+repeat :: a -> Stream a
+repeat x = x :> (repeat x)
 
---map :: (a -> b) -> (Stream a -> Stream b)
+map :: (a -> b) -> (Stream a -> Stream b)
+map f (a :> as) = (f a) :> map f as
 
---zipWith :: (a -> b -> c) -> (Stream a -> Stream b -> Stream c)
+zipWith :: (a -> b -> c) -> (Stream a -> Stream b -> Stream c)
+zipWith f (a:> as) (b:>bs) = (f a b) :> (zipWith f as bs)
 
---filter :: (a -> Bool) -> Stream a -> Stream a
+filter :: (a -> Bool) -> Stream a -> Stream a
+filter f (a:> as) = if f a then (a :> filter f as) else filter f as
 
---toList :: Stream a -> [a]
+{-
+3.)
 
---cycle :: [a] -> Stream a
+If we call filter (\x→False) (from 0), then the filtered stream will be empty but since it is infinite
+it will never terminate. So we will see an empty screen if we don't terminate manually.
+-}
+
+
+toList :: Stream a -> [a]
+toList (a :> as) = [a] ++ toList as
+
+cycle :: [a] -> Stream a
+cycle (x: xs)= x :> cycleHelper (x:xs) (xs)
+
+cycleHelper :: [a]-> [a] -> Stream a
+cycleHelper init [] = cycle init
+cycleHelper init (x:xs) = x :> (cycleHelper init xs)
 
 nat, fib :: Stream Integer
 nat = 0 :> zipWith (+) nat (repeat 1)
 fib = 0 :> 1 :> zipWith (+) fib (tail fib)
 
+primes :: Stream Integer
+primes = 
+
 --primetwins :: Stream (Integer,Integer)
+ 
 
 --combine :: Stream a -> Stream a -> Stream a
