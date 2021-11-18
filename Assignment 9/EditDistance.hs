3dd8bbdb53fb9f29da7228dc86250eb9af23d6c6
esari <sariege323@gmail.com>
2021-11-18 12:53:44 +0100

    Assignment 9 is finished

--- Assignment 9/EditDistance.hs
+++ Assignment 9/EditDistance.hs
@@ -1,3 +1,6 @@
+--Ege Sari s1034535
+--Group 81
+
 module EditDistance where
 
 import Data.Array
@@ -13,11 +16,15 @@ naiveEditDistance xs ys = distance xs ys
   cost x y = if x==y then 0 else 1
 
 
---editDistance :: String -> String -> Int
---editDistance xs ys = ...
---  where
---  ??? :: (Int,Int) -> Int
---
---  distArray :: Array (Int,Int) Int
---  distArray = ...
---
+editDistance :: String -> String -> Int
+editDistance xs ys = newDistance (0,0)
+  where
+  newDistance :: (Int,Int) -> Int
+  newDistance (x,y) 
+   | x>=length xs = (length ys-y)
+   | y>=length ys = (length xs-x)
+   | otherwise = minimum [(1 + distArray!(x+1,y)), (1 + distArray! (x,y+1)), ((cost (xs!!x) (ys!!y)) + distArray!(x+1,y+1) ) ]
+  cost x y = if x==y then 0 else 1
+
+  distArray :: Array (Int,Int) Int
+  distArray = array((0,0), (length xs, length ys ))[(ij, newDistance ij) | i <- [0..length xs], j <- [0..length ys], let ij = (i,j)]
