7295e65c9886c281ee769fa80ebc7679f007390c
ESari <sariege323@gmail.com>
2021-12-02 12:08:23 +0100

    Result.hs finished

--- Assignment 11/Result.hs
+++ Assignment 11/Result.hs
@@ -5,8 +5,16 @@ import Data.List
 data Result a = Okay a | Error [String]
   deriving (Eq,Ord,Show)
 
---instance Functor Result where
---  ...
+instance Functor Result where
+  --fmap :: (a -> b) -> Result a -> Result b
+  fmap _ (Error s) = Error s
+  fmap f (Okay a) = Okay (f a)
 
---instance Applicative Result where
---  ...
+instance Applicative Result where
+ -- (<*>) :: Result (a -> b) -> Result a -> Result b
+ --pure :: a -> Result a
+ pure a = Okay a
+ _ <*> (Error s) = Error s
+ (Error s) <*> _ = Error s
+ (Okay f) <*> (Okay a) = Okay (f a)
+  
