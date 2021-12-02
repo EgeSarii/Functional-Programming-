1db251f284c1a9f218e7488c5cd8e3dee48d0dca
ESari <sariege323@gmail.com>
2021-12-02 13:17:27 +0100

    Updated AST2hs

--- Assignment 11/AST2.hs
+++ Assignment 11/AST2.hs
@@ -8,23 +8,38 @@ type Identifier = String
 data Expr = Lit Integer | Var Identifier | Add Expr Expr | Sub Expr Expr | Mul Expr Expr | Div Expr Expr
   deriving (Show)
 
-eval :: (Fractional a, Eq a) => Expr -> [(Identifier,a)] -> Maybe a 
-eval (Lit k) _ = Just (fromInteger k) 
-eval (Add x y) vars = case (eval x vars, eval y vars) of 
-                     (Just x', Just y') -> Just (x'+y')
-                     _ -> Nothing
+data Result a = Okay a | Error [String]
+  deriving (Eq,Ord,Show)
+
+instance Functor Result where
+  --fmap :: (a -> b) -> Result a -> Result b
+  fmap _ (Error s) = Error s
+  fmap f (Okay a) = Okay (f a)
+
+instance Applicative Result where
+ -- (<*>) :: Result (a -> b) -> Result a -> Result b
+ --pure :: a -> Result a
+ pure a = Okay a
+ _ <*> (Error s) = Error s
+ (Error s) <*> _ = Error s
+ (Okay f) <*> (Okay a) = Okay (f a)
+  
+
+
+
+eval :: (Fractional a, Eq a) => Expr -> [(Identifier,a)] -> Result a 
+eval (Lit k) _ = pure (fromInteger k) 
+
+eval (Add x y) vars = (+) <$> (eval x vars) <*> (eval y vars)
                        
-eval (Sub x y) vars = case (eval x vars, eval y vars) of 
-                     (Just x', Just y') -> Just (x'-y')
-                     _ -> Nothing
+eval (Sub x y) vars = (-) <$> (eval x vars) <*> (eval y vars)
 
-eval (Mul x y) vars = case (eval x vars, eval y vars) of
-                     (Just x', Just y') -> Just (x'*y') 
-                     _ -> Nothing 
+eval (Mul x y) vars = (*) <$> (eval x vars) <*> (eval y vars)
 
-eval (Div x y) vars = case (eval x vars, eval y vars) of 
-                     (Just _, Just 0)  -> Nothing
-                     (Just x', Just y') -> Just (x'/y') 
-                     _ -> Nothing
+eval (Div x y) vars = if (eval y /= 0) then (/) <$> (eval x vars) <*> (eval y vars) 
+  else Error ["division by zero"]
 
-eval (Var name) vars = error "FIXME: variable support"
+eval (Var name) vars = case vars of
+                        [] -> Error["unknown variable: x","unknown variable: y"]
+                        (x:xs) -> if name == (fst x) then pure (snd x)
+                                  else eval (Var name) xs
