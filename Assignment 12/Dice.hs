ccdecb295d94f1ef5f4548e2bf3b6843b9d82bc3
ESari <sariege323@gmail.com>
2021-12-08 22:29:08 +0100

    Updated Dice.hs

--- Assignment 12/Dice.hs
+++ Assignment 12/Dice.hs
@@ -1,4 +1,4 @@
-module AST where
+module Dice where
 
 import System.Random
 import RandomState
@@ -12,15 +12,45 @@ infixl 6 :+:
 
 type DiceAction m = Integer -> m Integer
 
---evalM :: Expr -> DiceAction IO -> IO Integer  -- prototype
---evalM :: Expr -> DiceAction m -> m Integer    -- final version
+
+{-
+evalM :: Expr -> DiceAction IO -> IO Integer  -- prototype
+evalM (Lit i) da= return i
+evalM (Dice i) da= da i
+evalM (ex1 :+: ex2) da= (+) <$> (evalM ex1 da) <*> (evalM ex2 da)
+evalM (Min ex1 ex2) da= min <$> (evalM ex1 da) <*> (evalM ex2 da)
+evalM (Max ex1 ex2) da= max <$> (evalM ex1 da) <*> (evalM ex2 da)
+-}
+
+--fmap :: (a-> b) -> fa -> fb
+--pure :: a -> f a
+--(<*>) :: f (a -> b) -> f a -> f b
+--return :: a -> m a
+--(>>=) :: m a -> (a -> m b) -> m b
+--(>>) :: m a -> m b -> m b
+
+
+evalM ::(Monad m) => Expr -> DiceAction m -> m Integer -- final version
+-- final version
+evalM (Lit i) da= return i
+evalM (Dice i) da= da i
+evalM (ex1 :+: ex2) da= (+) <$> (evalM ex1 da) <*> (evalM ex2 da)
+evalM (Min ex1 ex2) da= min <$> (evalM ex1 da) <*> (evalM ex2 da)
+evalM (Max ex1 ex2) da= max <$> (evalM ex1 da) <*> (evalM ex2 da)
+
+
 
 --evalRIO :: Expr -> IO Integer
 --evalRIO expr = evalM expr (\dice->randomRIO (1,dice) >>= return) -- silent version
 --evalRIO expr = evalM expr (\dice->randomRIO (1,dice) >>= report) -- verbose version
 --  where report x = do { putStr "rolled a "; print x; return x }
 
---evalIO :: Expr -> IO Integer
+evalIO :: Expr -> IO Integer
+evalIO expr = evalM expr (\dice -> do {
+  putStrLn ("enter for Dice " ++ show dice); 
+  x <-getLine; 
+  return (read x)
+  })
 
 --evalND :: Expr -> [Integer]
 
