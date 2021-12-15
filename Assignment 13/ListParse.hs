e2c81b46bede8c53e3be339b3ee2d97603519f59
esari <sariege323@gmail.com>
2021-12-15 14:59:52 +0100

    ListParse.hs updated

--- Assignment 13/ListParse.hs
+++ Assignment 13/ListParse.hs
@@ -7,25 +7,16 @@ import Parser
 
 {- grammar:
  -   intList   = "{" { integer } "}"
- -}
---newtype Parser a = P { parse :: String -> Maybe (a, String) }
---parse :: Parser a -> (String -> Maybe (a, String))
---(<*>) :: f(a->b) -> fa -> fb
---fmap:: (a->b) -> fa -> fb
-parseAll :: Parser a -> String -> Maybe a
-parseAll p inp = case parse p inp of
-                   Just (x,[]) -> Just x
-                   _          -> Nothing
+-}
 
 intList :: Parser [Integer]
-intList = (pure (++)) <*> P(\s->  case s!!0 of
-                            '{' -> Just ([],  tail)
-                             '1' -> Just (1, tail)
-                             _ -> Nothing )
+intList = (:) <$> symbol "{" *> many natural <* symbol "}"
+
 
 {- grammar:
  -   intRecord = "{" integer "#" { integer } "}"
  -                   ^ =: n      ^^^^^^^^^^^ (repeat n# times)
  -}
 
---intRecord :: Parser [Integer]
+intRecord :: Parser [Integer]
+intRecord  = do { }
