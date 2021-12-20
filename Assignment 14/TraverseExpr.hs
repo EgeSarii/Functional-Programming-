a50ff27f3db144bc9946701b0c7ca02685de6ed4
ESari <sariege323@gmail.com>
2021-12-20 19:35:10 +0100

    TraverseExpr.hs is Updated

--- Assignment 14/TraverseExpr.hs
+++ Assignment 14/TraverseExpr.hs
@@ -12,9 +12,16 @@ data BinOp    = Add | Sub | Mul | Div
 
 instance Functor Expr where
   --fmap :: (a -> b) -> (Expr a -> Expr b)
+  fmap _ (Lit i) = Lit i
+  fmap f (Var var) =  Var (f var)
+  fmap f (Op op expr1 expr2) = Op op (fmap f expr1) (fmap f expr2)
+  
 
 instance Foldable Expr where
   --foldMap :: (Monoid m) => (a -> m) -> Expr a -> m
+  foldMap f (Lit i) = f i
+  foldMap f (Var var) = (f var)
+  foldMap f (Op op expr1 expr2) =  foldMap f expr1 <> foldMap f expr2
 
 instance Traversable Expr where
   --traverse :: (Applicative f) => (a -> f b) -> Expr a -> f (Expr b)
