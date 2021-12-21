a2f42b5d25cc9d6d162f2a0eae22b3ee46f8dc40
ESari <sariege323@gmail.com>
2021-12-21 21:36:30 +0100

    TraverseExpr.hs is Updated

--- Assignment 14/TraverseExpr.hs
+++ Assignment 14/TraverseExpr.hs
@@ -19,16 +19,31 @@ instance Functor Expr where
 
 instance Foldable Expr where
   --foldMap :: (Monoid m) => (a -> m) -> Expr a -> m
-  foldMap f (Lit i) = f i
+  foldMap f (Lit i) = mempty
   foldMap f (Var var) = (f var)
   foldMap f (Op op expr1 expr2) =  foldMap f expr1 <> foldMap f expr2
 
 instance Traversable Expr where
   --traverse :: (Applicative f) => (a -> f b) -> Expr a -> f (Expr b)
+  traverse f (Lit i) = pure(Lit i)
+  traverse f (Var var) = pure(\x -> (Var x)) <*> (f var)
+  traverse f (Op op expr1 expr2) = pure (Op op) <*> (traverse f expr1) <*> (traverse f expr2)
 
---allVars :: (Ord a) => Expr a -> [a]
+allVars :: (Ord a) => Expr a -> [a]
+allVars expr = nub (foldMap (:[]) expr)
+
+{-
+return :: a → State s a,
+get :: State s s
+put :: s → State s ()
+-}
 
 --renameVar :: String -> State [(String,Int)] Int
---renameVar name = do ...
+--renameVar name = do var <- get 0
+  --                  if (v)
+
+renameHelper :: String -> Int -> State [(String,Int)] Int
+renameHelper name index = do var <- get index
+                             if (name == var ) then return 
 
 --indexVars :: Expr String -> Expr Int
